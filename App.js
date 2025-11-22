ee3cb2584f2903761440dbe996ce2173c4da6377
quintonmills <quintonmills98@gmail.com>
2025-11-22 11:11:01 -0500

    add navigation stack

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import React from 'react';
 import LoginScreen from './src/screens/login';
+import ContestSelectionScreen from './src/screens/ContestSelectionScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen
+          name='Login'
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name='ContestSelectionScreen'
+          component={ContestSelectionScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
