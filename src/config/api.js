689150a5eaa155c03c28f2a624c46f6d62dc930e
quintonmills <quintonmills98@gmail.com>
2025-11-30 06:13:18 -0500

    add global api config

---
+++ src/config/api.js
@@ -0,0 +1,6 @@
+// src/config/api.js
+
+// Your computer's LAN IP address (NOT 127.0.0.1, NOT localhost)
+// Example: ipconfig getifaddr en0
+export const API_URL =
+  'https://metamorphous-doretha-transpleural.ngrok-free.dev';
