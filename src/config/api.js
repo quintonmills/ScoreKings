2afdd5978f0bf68dbf53ac5217b70be625f2e23f
quintonmills <quintonmills98@gmail.com>
2025-12-24 10:28:31 -0500

    update api url to point at render

--- src/config/api.js
+++ src/config/api.js
@@ -2,4 +2,4 @@
 
 // Your computer's LAN IP address (NOT 127.0.0.1, NOT localhost)
 // Example: ipconfig getifaddr en0
-export const API_URL = 'http://172.16.225.115:4000';
+export const API_URL = 'https://server-core-1.onrender.com';
