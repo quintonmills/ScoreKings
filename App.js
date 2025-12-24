44ced9371305a3cbfdc8a37a1dfb33e48102b210
quintonmills <quintonmills98@gmail.com>
2025-12-24 12:11:26 -0500

    test stataement

--- App.js
+++ App.js
@@ -1,48 +1,17 @@
-import LoginScreen from './src/screens/Login';
-import PlayerSelection from './src/screens/PlayerSelection';
-import ContestReviewScreen from './src/screens/ContestReview';
-import PaymentScreen from './src/screens/Payment';
-import MainTabs from './src/navigation/MainTabs';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
-
 export default function App() {
+  // BYPASS EVERYTHING
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen
-          name='Login'
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-
-        {/* After login, go into Tabs */}
-        <Stack.Screen
-          name='MainTabs'
-          component={MainTabs}
-          options={{ headerShown: false }}
-        />
-
-        {/* These stay above tabs */}
-        <Stack.Screen
-          name='PlayerSelection'
-          component={PlayerSelection}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='ContestReviewScreen'
-          component={ContestReviewScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='PaymentScreen'
-          component={PaymentScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'blue',
+      }}
+    >
+      <Text style={{ color: 'white' }}>
+        IF YOU SEE THIS, THE NAVIGATOR IS THE PROBLEM
+      </Text>
+    </View>
   );
 }
