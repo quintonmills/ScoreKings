141a87b6c801ba5cf2ea50752f98c47e854b186c
quintonmills <quintonmills98@gmail.com>
2025-12-28 14:48:28 -0500

    use api url from env

--- src/config/api.js
+++ src/config/api.js
@@ -2,4 +2,5 @@
 
 // Your computer's LAN IP address (NOT 127.0.0.1, NOT localhost)
 // Example: ipconfig getifaddr en0
-export const API_URL = 'https://server-core-1.onrender.com/api';
+export const API_URL =
+  process.env.EXPO_PUBLIC_API_URL || 'https://server-core-1.onrender.com/api';
